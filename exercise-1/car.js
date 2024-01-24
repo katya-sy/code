c5a0f170f9441525ffb267733dfbc96a35fbf5ca
katya-sy <katya_sy_28@mail.ru>
2024-01-24 12:35:00 +0900

    1, 2, 3, 4 exercises in 1 exercise

---
+++ exercise-1/car.js
@@ -0,0 +1,25 @@
+const carName = "Toyota";
+let maxCarSpeed = 140,
+  carOwner = "Ivan";
+
+alert(carName);
+alert(maxCarSpeed);
+alert(carOwner);
+
+maxCarSpeed = 150;
+console.log("Изменилась скорость", maxCarSpeed);
+
+carOwner = "Anna";
+console.log("Изменился владелец", carOwner);
+
+maxCarSpeed = 130;
+console.log("Изменилась скорость", maxCarSpeed);
+
+carOwner = "Oleg";
+console.log("Изменился владелец", carOwner);
+
+maxCarSpeed = 125;
+console.log("Изменилась скорость", maxCarSpeed);
+
+carOwner = "Kto-to";
+console.log("Изменился владелец", carOwner);
